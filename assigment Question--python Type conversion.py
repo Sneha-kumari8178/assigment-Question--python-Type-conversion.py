33e5b5bf3d5635fc7b1012ad8ddefe4fe6434cbb
Sneha-kumari8178 <163983112+Sneha-kumari8178@users.noreply.github.com>
2024-05-03 20:36:36 +0530

    Add files via upload

---
+++ assigment Question--python Type conversion.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+# 1) convert integer to a floating--point number.
+
+# Frist question
+num =float(4)
+type(num)
+
+
+# In[7]:
+
+
+# 2) convert a float to an Integer?
+
+num1 =int(4.5)
+type(num1)
+
+
+# In[9]:
+
+
+# 3) convert an integer to a string?
+
+value =str(45)
+type(value)
+
+
+# In[12]:
+
+
+# 4) convert a List to a Tuple?
+
+list1 =tuple(["shyam ji","Ram","kaam",1,2,3,"True"])
+type(list1)
+
+
+# In[22]:
+
+
+# 5) convert a Tuple to a List?
+
+value1 =list(('Ram','rohan',"kaam",1,2,3,4,True))
+type(value1)
+
+
+# In[14]:
+
+
+# 6) convert a decimal number to binary?
+
+num = 7
+print(bin(num))
+type(num)         
+
+
+# In[18]:
+
+
+# 8) convert a non- zero number to boolean.
+
+num2 = bool(45)
+print(num2)
+
+
+# In[ ]:
+
+
+
+
